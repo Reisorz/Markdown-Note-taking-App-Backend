db84a43e2108bd5e730b1c74fb70bf6288ad3e84
Reisorz <166374263+Reisorz@users.noreply.github.com>
2025-01-10 11:11:00 +0100

    First commit

---
+++ src/main/java/com/mls/Markdown/Note_taking/App/Backend/MarkdownNoteTakingAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mls.Markdown.Note_taking.App.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdownNoteTakingAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkdownNoteTakingAppBackendApplication.class, args);
+	}
+
+}
